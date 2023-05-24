61ae7c7e68755417bb3d1651fe61d3ffec3ae2b5
YijieWang <yijwang@thoughtworks.com>
2023-05-24 13:12:41 +0800

    278. First Bad Version

---
+++ src/main/java/_278_First_Bad_Version/VersionControl.java
@@ -0,0 +1,8 @@
+package _278_First_Bad_Version;
+
+public class VersionControl {
+  boolean isBadVersion(int n) {
+    return n == 2;
+  }
+
+}
---
+++ src/test/java/_278_First_Bad_Version/SolutionTest.java
@@ -0,0 +1,13 @@
+package _278_First_Bad_Version;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+  @Test
+  void name() {
+    Solution solution = new Solution();
+    solution.firstBadVersion(2);
+  }
+}
