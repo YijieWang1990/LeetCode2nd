c7bf13484b4f41bc54c8c3cd26f167be2d10a054
YijieWang <yijwang@thoughtworks.com>
2023-06-17 17:44:40 +0800

    560. Subarray Sum Equals K

---
+++ src/test/java/_560_Subarray_Sum_Equals_K/SolutionTest.java
@@ -0,0 +1,13 @@
+package _560_Subarray_Sum_Equals_K;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+  @Test
+  void name() {
+    Solution solution = new Solution();
+    solution.subarraySum(new int[]{1, 1, 1}, 2);
+  }
+}
